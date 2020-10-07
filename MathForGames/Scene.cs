7dd5583c3d11bfdda7241e0dc5b6900b82f00eea
jeremyjflowers <rpgjuwangaming@gmail.com>
2020-10-07 17:03:34 -0500

    Added Entity class. Added Scene class.

---
+++ MathForGames/Scene.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathForGames
+{
+    class Scene
+    {
+        private Entity[] _entities;
+        
+        public Scene()
+        {
+            _entities = new Entity[0];
+        }
+
+        public void AddEntity(Entity entity)
+        {
+            Entity[] appendedArray = new Entity[_entities.Length + 1];
+            for(int i = 0; i < _entities.Length; i++)
+            {
+                appendedArray[i] = _entities[1];
+            }
+            appendedArray[_entities.Length] = entity;
+            _entities = appendedArray;
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+    }
+}
